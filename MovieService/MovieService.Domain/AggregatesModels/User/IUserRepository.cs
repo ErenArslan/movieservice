3b9295c26311ef308dd06f6c07dbc7f5035a61f1
eren <eren.arslan6@gmail.com>
2020-08-21 13:58:51 +0300

    User repository login edited

--- MovieService/MovieService.Domain/AggregatesModels/User/IUserRepository.cs
+++ MovieService/MovieService.Domain/AggregatesModels/User/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace MovieService.Domain.AggregatesModels.User
 {
     interface IUserRepository:IRepository<User>
     {
-        Task<User> Login(string username, string password);
+        Task<User> Login(string identifier, string password);
         Task<User> Register(User user);
 
     }
