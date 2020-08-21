0a2e8bfdc8f3f0817b5b3e70f80fe315d15c7d94
eren <eren.arslan6@gmail.com>
2020-08-21 15:04:52 +0300

    MovieFetchedIntegrationEvent implemented

--- MovieService/MovieService.Application/IntegrationEvents/Events/MovieFetchedIntegrationEvent.cs
+++ MovieService/MovieService.Application/IntegrationEvents/Events/MovieFetchedIntegrationEvent.cs
@@ -1,8 +1,33 @@
-﻿using DotNetCore.CAP;
+﻿using System;
 
 namespace MovieService.Application.IntegrationEvents.Events
 {
     public class MovieFetchedIntegrationEvent 
     {
+        public int Id { get; set; }
+        public double Popularity { get; private set; }
+        public string PosterPath { get; private set; }
+        public bool IsAdult { get; private set; }
+        public string BackdropPath { get; private set; }
+        public string OriginalLanguage { get; private set; }
+        public string OriginalTitle { get; private set; }
+        public string Title { get; private set; }
+        public string Overview { get; private set; }
+        public DateTime ReleaseDate { get; private set; }
+
+        public MovieFetchedIntegrationEvent(int id,double popularity,string posterPath,bool isAdult,string backdropPath,
+           string originalLanguage,string originalTitle,string title,string overview,DateTime releaseDate)
+        {
+            Id = id;
+            Popularity = popularity;
+            PosterPath = posterPath;
+            IsAdult = isAdult;
+            BackdropPath = backdropPath;
+            OriginalLanguage = originalLanguage;
+            OriginalTitle = originalTitle;
+            Title = title;
+            Overview = overview;
+            ReleaseDate = releaseDate;
+        }
     }
 }
